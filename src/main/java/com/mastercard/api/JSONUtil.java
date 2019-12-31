ab6cc43fba1218ce705c757195f5f62455f4d693
Ygritte <ygritte.zhu@3ti.us>
2019-12-31 16:42:44 +0800

    dd

--- src/main/java/com/mastercard/api/JSONUtil.java
+++ src/main/java/com/mastercard/api/JSONUtil.java
@@ -8,25 +8,25 @@ import java.util.*;
 
 public class JSONUtil {
 
-    public static <T> String listToJson(List<T> ts) {
+    /*public static <T> String listToJson(List<T> ts) {
         String jsons = JSON.toJSONString(ts);
         return jsons;
     }
-
+*/
     /**
      * json 转 List<T>
      */
-    public static <T> List<T> jsonToList(String jsonString, Class<T> clazz) {
+    /*public static <T> List<T> jsonToList(String jsonString, Class<T> clazz) {
         @SuppressWarnings("unchecked")
         List<T> ts = (List<T>) JSONArray.parseArray(jsonString, clazz);
         return ts;
-    }
+    }*/
 
 
-    public static List<OfferResp> jsonToObject(String offer) {
+   /* public static List<OfferResp> jsonToObject(String offer) {
         JSONObject jsonObject = JSON.parseObject(offer);
         String data = jsonObject.getObject("data", String.class);
         List<OfferResp> offerResps = jsonToList(data, OfferResp.class);
         return offerResps;
-    }
+    }*/
 }
